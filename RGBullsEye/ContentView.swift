820766ae3c0cc66afe212e1f20c3b98263a19179
saj panchal <sajpanchal2020@outlook.com>
2022-01-16 11:46:24 -0500

    added an alert view to show score after button tap. modified color view with a new subview to represent both.

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -35,18 +35,34 @@ import SwiftUI
 struct ContentView: View {
     @State var game = Game()
     @State var guess: RGB
+    @State var showScore = false
   var body: some View {
       VStack {
-          Color(rgbStruct: game.target)
-          Text("R: ??? G: ??? B: ???")
-              .padding()
-          Color(rgbStruct: guess)
+          ColorCircle(color: game.target)
+         
+          if !showScore {
+              Text("R: ??? G: ??? B: ???")
+                  .padding()
+          } else {
+              Text(game.target.intString())
+                  .padding()
+          }
+          ColorCircle(color: guess)
+         
           Text(guess.intString())
               .padding()
           ColorSlider(value: $guess.red, trackColor: .red)
           ColorSlider(value: $guess.green, trackColor: .green)
           ColorSlider(value: $guess.blue, trackColor: .blue)
           Button("Hit Me") {
+              showScore = true
+              game.check(guess: guess)
+          }
+          .alert(isPresented: $showScore) {
+              Alert(title: Text("Your Score"), message: Text(String(game.scoreRound)), dismissButton: .default(Text("OK")) {
+                  game.startNewRound()
+                  guess = RGB()
+              })
           }
       }
   }
@@ -72,3 +88,11 @@ struct ColorSlider: View {
     }
 }
 
+
+struct ColorCircle: View {
+    var color: RGB
+    var body: some View {
+        Circle()
+            .fill(Color(rgbStruct: color))
+    }
+}
