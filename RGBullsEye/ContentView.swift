4a5ce2b6af5eaf9cd01a75dc9903b64f74f678ed
saj panchal <sajpanchal2020@outlook.com>
2022-02-05 12:12:36 -0500

    changed font size for entire app. Enclosed the screen parent container inside a GeometryReader to access size and frame of any iPhone Screen. applied dynamic size to UI elements to make it fit to any iPhone screen.

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -36,39 +36,47 @@ struct ContentView: View {
     @State var game = Game()
     @State var guess: RGB
     @State var showScore = false
+    let circleSize: CGFloat = 0.275
+    let labelHeight: CGFloat = 0.06
+    let labelWidth: CGFloat = 0.53
+    let buttonWidth: CGFloat = 0.87
+    
   var body: some View {
       
-      ZStack {
-          Color.element
-            .ignoresSafeArea(.all)
-          VStack {
-              ColorCircle(rgb: game.target, size: 150)
-              
-              if !showScore {
-                  BevelText(text: "R: ??? G: ??? B: ???", width: 200, height: 48)
-                 
-              } else {
-                  BevelText(text: game.target.intString(), width: 200, height: 48)
+      GeometryReader { proxy in
+          ZStack {
+              Color.element
+                .ignoresSafeArea(.all)
+              VStack {
+                  ColorCircle(rgb: game.target, size: proxy.size.height * circleSize)
+                  
+                  if !showScore {
+                      BevelText(text: "R: ??? G: ??? B: ???", width: proxy.size.width * labelWidth, height: proxy.size.height * labelHeight)
+                     
+                  } else {
+                      BevelText(text: game.target.intString(), width: proxy.size.width * labelWidth, height: proxy.size.height * labelHeight)
+                  }
+                  ColorCircle(rgb: guess, size: proxy.size.height * circleSize)
+                  
+                  BevelText(text: guess.intString(), width: proxy.size.width * labelWidth, height: proxy.size.height * labelHeight)
+                  
+                  ColorSlider(value: $guess.red, trackColor: .red)
+                  ColorSlider(value: $guess.green, trackColor: .green)
+                  ColorSlider(value: $guess.blue, trackColor: .blue)
+                  
+                  Button("Hit Me") {
+                      showScore = true
+                      game.check(guess: guess)
+                  }
+                  .buttonStyle(NeuButtonStyle(width: proxy.size.width * buttonWidth, height: proxy.size.height * labelHeight))
+                  .alert(isPresented: $showScore) {
+                      Alert(title: Text("Your Score"), message: Text(String(game.scoreRound)), dismissButton: .default(Text("OK")) {
+                          game.startNewRound()
+                          guess = RGB()
+                      })
+                  }
               }
-              ColorCircle(rgb: guess, size: 150)
-              
-              BevelText(text: guess.intString(), width: 200, height: 48)
-              
-              ColorSlider(value: $guess.red, trackColor: .red)
-              ColorSlider(value: $guess.green, trackColor: .green)
-              ColorSlider(value: $guess.blue, trackColor: .blue)
-              
-              Button("Hit Me") {
-                  showScore = true
-                  game.check(guess: guess)
-              }
-              .buttonStyle(NeuButtonStyle(width: 327, height: 48))
-              .alert(isPresented: $showScore) {
-                  Alert(title: Text("Your Score"), message: Text(String(game.scoreRound)), dismissButton: .default(Text("OK")) {
-                      game.startNewRound()
-                      guess = RGB()
-                  })
-              }
+              .font(.headline)
           }
       }
   }
@@ -76,7 +84,13 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-      ContentView(guess: RGB())
+      Group {
+          ContentView(guess: RGB())
+              .previewDevice("iPhone 11")
+          ContentView(guess: RGB())
+              .previewDevice("iPhone SE (2nd generation)")
+         
+      }
   }
 }
 
@@ -91,6 +105,7 @@ struct ColorSlider: View {
             Text("255")
         }
         .padding(.horizontal)
+        .font(.subheadline)
     }
 }
 
